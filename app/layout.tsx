374b8407166137b179119f4c8d5a08e210bc6ac6
kapepa <kapepayx@gmail.com>
2024-05-20 11:31:04 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         >
           <ModalProvider/>
           <ToastProvider/>
-          <Navbar/>
+          {/* <Navbar/> */}
             {children}
           <Footer/>
         </ThemeProvider>
